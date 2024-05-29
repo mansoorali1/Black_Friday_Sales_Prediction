0ccb4c6eed953f5044eb39921e2f22fd83b1adf0
mansoorali1 <mansoorali.shaik23@gmail.com>
2024-05-29 23:00:29 +0530

    file updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="black_friday",
+    version="0.0.0",
+    author="Mansoor",
+    author_email="mansoorali.shaik23@gmail.com",
+    packages=find_packages()
+)
