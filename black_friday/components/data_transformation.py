c3dae357ba6203166a6be21724d8e341f0a5fe89
mansoorali1 <mansoorali.shaik23@gmail.com>
2024-06-04 23:12:45 +0530

    file updated

--- black_friday/components/data_transformation.py
+++ black_friday/components/data_transformation.py
@@ -11,7 +11,7 @@ from black_friday.constants import TARGET_COLUMN, SCHEMA_FILE_PATH, CURRENT_YEAR
 from black_friday.entity.config_entity import DataTransformationConfig
 from black_friday.entity.artifact_entity import DataTransformationArtifact, DataIngestionArtifact, DataValidationArtifact
 from black_friday.exception import USvisaException
-from us_visa.logger import logging
+from black_friday.logger import logging
 from us_visa.utils.main_utils import save_object, save_numpy_array_data, read_yaml_file, drop_columns
 from us_visa.entity.estimator import TargetValueMapping
 
